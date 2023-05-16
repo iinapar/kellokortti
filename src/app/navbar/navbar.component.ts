fa1c058d3781ec3a76adedb0e7e1e6e9b0722408
Iina Parikainen <ab7383@student.jamk.fi>
2023-05-16 10:03:32 +0300

    navbar muokattu

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  constructor(private authService: AuthService) {}
 
+  logOut() {
+    this.authService.isLogged = false;
+  }
 }
