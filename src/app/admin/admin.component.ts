f10251b1c10d1fd4f57888d74a9bbe50b481339c
Iina Parikainen <ab7383@student.jamk.fi>
2023-05-16 13:36:30 +0300

    admin säätöä

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent {
+  workers = [
+    { etunimi: 'Marja', sukunimi: 'Sirpaleena' },
+    { etunimi: 'Esko', sukunimi: 'Markkanen' },
+    { etunimi: 'Markku', sukunimi: 'Petteri' },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {
+    // this.getWorkers();
+  }
+
+  // getWorkers(): void {
+  //   this.workerService.getWorkers().subscribe((workers) => (this.workers = workers));
+  // }
+  //  add(name: string): void {
+  //   name = name.trim();
+  //   if (!name) {
+  //     return;
+  //   }
+  //   this.workerService.addWorker({ name }).subscribe((worker) => {
+  //     this.workers.push(worker);
+  //   });
+  // }
+
+  // delete(worker: Worker): void {
+  //   this.workers = this.workers.filter((h) => h !== worker);
+  //   this.workerService.deleteHero(worker.id).subscribe();
+  // }
 }
