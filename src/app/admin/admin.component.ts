d3a99705a1ac27300bb3ebf5ed306e1bcf7ba27f
Iina Parikainen <ab7383@student.jamk.fi>
2023-05-17 09:45:42 +0300

    työntekijän muokkaus lisätty

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -14,6 +14,11 @@ export class AdminComponent {
   name = '';
   lastname = '';
   kirjautumiset = false;
+  id = 3;
+  editId = 0;
+  addnew = true;
+  saveedited = false;
+  editKirjautumiset = {};
 
   constructor(private workerService: WorkerService) {}
 
@@ -31,33 +36,65 @@ export class AdminComponent {
 
   addW() {
     this.addWorker = !this.addWorker;
+    this.addnew = true;
   }
 
   onSubmit(f: any) {
-    this.workerService
-      .addWorker({
-        id: 3,
-        tunnus: f.tunnus,
-        etunimi: f.name,
-        sukunimi: f.lastname,
-        kirjautumiset: [
-          {
-            sisaan: '',
-            ulos: '',
-            taukoAlku: '',
-            taukoLoppu: '',
-          },
-        ],
-      })
-      .subscribe((worker) => {
-        this.workers.push(worker);
-      });
+    if (this.addnew === true) {
+      this.id = this.id + 1;
+      this.workerService
+        .addWorker({
+          id: this.id,
+          tunnus: f.tunnus,
+          etunimi: f.name,
+          sukunimi: f.lastname,
+          kirjautumiset: [
+            {
+              sisaan: '',
+              ulos: '',
+              taukoAlku: '',
+              taukoLoppu: '',
+            },
+          ],
+        })
+        .subscribe((worker) => {
+          this.workers.push(worker);
+        });
 
-    this.addWorker = !this.addWorker;
+      this.addWorker = !this.addWorker;
+    }
+
+    if (this.saveedited === true) {
+      this.workerService
+        .updateWorker({
+          id: this.editId,
+          tunnus: f.tunnus,
+          etunimi: f.name,
+          sukunimi: f.lastname,
+          kirjautumiset: this.editKirjautumiset,
+        })
+        .subscribe(() => this.getWorkers());
+      this.tunnus = '';
+      this.saveedited = false;
+      this.name = '';
+      this.lastname = '';
+      this.addWorker = false;
+    }
   }
 
   delete(worker: worker): void {
     this.workers = this.workers.filter((h) => h !== worker);
     this.workerService.deleteHero(worker.tunnus).subscribe();
+  }
+
+  update(w: worker) {
+    this.tunnus = w.tunnus;
+    this.name = w.etunimi;
+    this.lastname = w.sukunimi;
+    this.addnew = !this.addnew;
+    this.saveedited = !this.saveedited;
+    this.addWorker = !this.addWorker;
+    this.editId = w.id;
+    this.editKirjautumiset = w.kirjautumiset;
   }
 }
